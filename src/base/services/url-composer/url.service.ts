f2f809432424981a84442edd37dd8d29093d9bea
Rafał <lafarinio@github.com>
2019-10-30 11:03:44 +0100

    UrlService: preparations

--- src/base/services/url-composer/url.service.ts
+++ src/base/services/url-composer/url.service.ts
@@ -1,9 +1,20 @@
 import { Injectable } from '@angular/core';
+import { finalEnvironment } from '../../../environments/final.environment';
+import { keyBy } from 'lodash';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UrlService {
 
-  constructor() { }
+  constructor() {}
+
+  private getUrlPath(urlKey: string, params?: object) {
+    const urlList = keyBy(finalEnvironment.urlsConfiguration);
+    const urlParams = params && keyBy(params);
+  }
+
+  private getBaseUrl(): string {
+    return finalEnvironment.baseUrl;
+  }
 }
--- src/environments/final.environment.ts
+++ src/environments/final.environment.ts
@@ -1,4 +1,4 @@
 import { urlsConfiguration } from './base.environment';
 import { environment } from './environment';
 
-export const finalEnvironment = {...urlsConfiguration, ...environment};
+export const finalEnvironment = {urlsConfiguration, ...environment};
