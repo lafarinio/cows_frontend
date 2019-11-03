cda638b25fe80e29327af184a90ff58d93e6cb9e
Rafał <lafarinio@github.com>
2019-11-03 12:04:11 +0100

    TimeSelector: basic component created

--- src/app/modules/modules.module.ts
+++ src/app/modules/modules.module.ts
@@ -1,11 +1,16 @@
 import { NgModule } from '@angular/core';
 import { ToolbarModule } from './toolbar/toolbar.module';
+import { TimeSelectorComponent } from './time-selector/time-selector.component';
 
 
 
 @NgModule({
   imports: [
     ToolbarModule
-  ]
+  ],
+  exports: [
+    TimeSelectorComponent
+  ],
+  declarations: [TimeSelectorComponent]
 })
 export class ModulesModule { }
---
+++ src/app/modules/time-selector/time-selector.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, OnInit, Output } from '@angular/core';
+
+@Component({
+  selector: 'cows-time-selector',
+  templateUrl: './time-selector.component.html'
+})
+export class TimeSelectorComponent implements OnInit {
+
+  @Output()
+  timeEventEmitter: EventEmitter<Date>;
+
+  constructor() { }
+
+  ngOnInit() {
+    this.timeEventEmitter = new EventEmitter<Date>();
+  }
+
+  emitTime(time: Date) {
+    this.timeEventEmitter.emit(time);
+  }
+
+  testTimeGenerator(): Date {
+    return new Date();
+  }
+
+}
