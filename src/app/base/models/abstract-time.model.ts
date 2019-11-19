c53dd28e6d6742de90213bf1fbc7e3188b361145
Rafał <226467@student.pwr.edu.pl>
2019-11-19 20:38:14 +0100

    StrictCowPosition: refactor; added AbstractTime model

---
+++ src/app/base/models/abstract-time.model.ts
@@ -0,0 +1,4 @@
+
+export interface AbstractTime {
+  time: Date;
+}
--- src/app/modules/charts/models/stricted-cow-position.model.ts
+++ src/app/modules/charts/models/stricted-cow-position.model.ts
@@ -1,9 +1,10 @@
 import { serialize, deserialize, serializeAs, deserializeAs, inheritSerialization } from 'cerialize';
 import { dateSerializer } from '../../../base/serializers/date-serializer';
 import { Position } from './position.model';
+import { AbstractTime } from '../../../base/models/abstract-time.model';
 
 @inheritSerialization(Position)
-export class StrictedCowPosition extends Position {
+export class StrictedCowPosition extends Position implements AbstractTime {
   @serialize
   @deserialize
   id: string;
