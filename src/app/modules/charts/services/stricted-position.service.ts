bf0572ebbb4d60bda53ec5e07d9efebd56a34e2f
Rafał <226467@student.pwr.edu.pl>
2019-12-07 14:09:15 +0100

    StrictedPosition: created service

---
+++ src/app/modules/charts/services/stricted-position.service.ts
@@ -0,0 +1,28 @@
+import { Injectable } from '@angular/core';
+import { UrlService } from '../../../base/services/url.service';
+import { Observable } from 'rxjs';
+import { StrictedCowPosition } from '../models/stricted-cow-position.model';
+import { HttpClient } from '@angular/common/http';
+import { map } from 'rxjs/operators';
+import { Deserialize } from 'cerialize';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class StrictedPositionService {
+
+  constructor(private ulrService: UrlService,
+              private http: HttpClient) { }
+
+  getCows(): Observable<Array<StrictedCowPosition>> {
+    const path = 'cowLists';
+    const url = this.ulrService.getUrl(path);
+    console.log(path, url);
+    return this.http.get(url).pipe(
+      map(response => {
+        console.log(response);
+        return Deserialize(response, StrictedCowPosition);
+      })
+    );
+  }
+}
