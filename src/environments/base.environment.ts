698c333865ad8441ae2f696c86651e1af08053f5
Rafał <226467@student.pwr.edu.pl>
2020-01-12 15:49:31 +0100

    lista NOWYCH URL do bazy

--- src/environments/base.environment.ts
+++ src/environments/base.environment.ts
@@ -1,5 +1,13 @@
 
 export const urlsConfiguration = {
+  // stare, do usuniecia
   cowLists: 'cows',
-  cow: 'cows/:id'
+  cow: 'cows/:id',
+  // nowe
+  firstAlgorithm: 'timeLocation/getFirstAlgorithm/:idCowShed',
+  thirdAlgorithm: 'timeLocation/getThirdAlgorithm/:idCowShed',
+  getCowSheds: 'cowShed/getAll',
+  getCowShed: 'cowShed/getCowShed/:id',
+  getCows: 'cow/getAll',
+  getCow: 'cow/getCow/:id'
 };
