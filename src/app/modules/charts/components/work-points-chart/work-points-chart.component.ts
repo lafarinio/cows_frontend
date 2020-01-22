08bc86bff546f8cf25955bedf611ef198a1af16a
Rafał <226467@student.pwr.edu.pl>
2020-01-22 17:37:58 +0100

    refactor to methods

--- src/app/modules/charts/components/work-points-chart/work-points-chart.component.ts
+++ src/app/modules/charts/components/work-points-chart/work-points-chart.component.ts
@@ -6,26 +6,9 @@ import am4themes_material from '@amcharts/amcharts4/themes/material';
 import { PositionNames } from '../../models/position.model';
 import { WorkpointAreaPosition } from '../../models/workpoint-area-position.model';
 import { UrlService } from '../../../../base/services/url.service';
+import { CowshedData } from "../../models/CowshedData.model";
 
-function barnToChartScalers(bWidth: number, bHeight: number, bX: number, bY: number) {
-  const sX = bX / bWidth;
-  const sY = bY / bHeight;
 
-  return [sX, sY];
-}
-
-function barnCoordToChartCoord(bWidth: number, bHeight: number, cWidth: number, cHeight: number, bX: number, bY: number) {
-  // assumes barn coords go from (0,0) in the bottom left, (maxX, maxY) in the top right
-
-  let sX: number;
-  let sY: number;
-  [sX, sY] = barnToChartScalers(bWidth, bHeight, bX, bY);
-
-  const cX = cWidth - cWidth * sX;
-  const cY = cHeight - cHeight * sY;
-
-  return [cX, cY];
-}
 
 const barnWidth = 30;
 const barnHeight = 20;
@@ -37,7 +20,11 @@ const barnHeight = 20;
 })
 export class WorkPointsChartComponent implements OnInit, OnDestroy {
   private chart: am4charts.XYChart;
-
+  private data: Array<any>;
+  private sensors: any;
+  private selectedBarn: CowshedData;
+  private contentHeight = 428.67;
+  private contentWidth = 742;
   constructor(private zone: NgZone,
               private urlService: UrlService) {
   }
@@ -52,153 +39,162 @@ export class WorkPointsChartComponent implements OnInit, OnDestroy {
   }
 
   ngOnInit() {
-    // const example = this.urlService.getUrl('cow', {id: '123'});
-    // console.log(example);
-    // const example2 = this.urlService.getUrl('cowLists');
-    // console.log(example2);
     this.zone.runOutsideAngular(() => {
-      // Create chart instance
-      const chart = am4core.create('chartdiv', am4charts.XYChart);
-      chart.colors.step = 3;
+      this.initializeChart();
 
-      // Create axes
-      const xAxis = chart.xAxes.push(new am4charts.ValueAxis());
-      xAxis.renderer.minGridDistance = 50;
-      xAxis.min = 0;
-      xAxis.max = barnWidth;
+      this.initializeData();
+      this.chart.data = this.data;
 
-      const yAxis = chart.yAxes.push(new am4charts.ValueAxis());
-      yAxis.renderer.minGridDistance = 50;
-      yAxis.min = 0;
-      yAxis.max = barnHeight;
+      this.initializeSensors();
+      this.drawSensors();
 
-      const bgColor = new am4core.InterfaceColorSet().getFor('background');
-
-      // Create series #1
-      const series = chart.series.push(new am4charts.LineSeries());
-      series.dataFields.valueY = PositionNames.posY;
-      series.dataFields.valueX = PositionNames.posX;
-      series.dataFields.value = PositionNames.valueInWP;
-      series.strokeOpacity = 0;
-      series.name = 'Series 1';
-
-      const bullet = series.bullets.push(new am4core.Rectangle());
-      bullet.tooltipText = `X{${PositionNames.posY}} Y{${PositionNames.posX}} : {value.workingValue.formatNumber("#.")} cows`;
-
-      bullet.width = 10;
-      bullet.height = 10;
-
-      series.heatRules.push({
-        target: bullet,
-        property: 'fill',
-        minValue: 0,
-        maxValue: 20,
-        min: am4core.color(bgColor),
-        max: chart.colors.getIndex(0)
-      });
-
-      const contentHeight = 428.67;
-      const contentWidth = 742;
-
-      bullet.adapter.add('pixelHeight', (pixelHeight, target) => {
-        const dataItem: any = target.dataItem;
-
-        const scalers = barnToChartScalers(barnWidth, barnHeight, dataItem.dataContext.wallpointWidth, dataItem.dataContext.wallpointHeight);
-        const scaledHeight = scalers[1] * contentHeight;
-
-        return scaledHeight;
-      });
-
-      bullet.adapter.add('pixelWidth', (pixelWidth, target) => {
-        const dataItem: any = target.dataItem;
-
-        const scalers = barnToChartScalers(barnWidth, barnHeight, dataItem.dataContext.wallpointWidth, dataItem.dataContext.wallpointHeight);
-        const scaledWidth = scalers[0] * contentWidth;
-
-        return scaledWidth;
-      });
-
-      const data = [
-        {
-          // from lower left corner
-          posX: 0,
-          posY: 10,
-          wallpointWidth: 20,
-          wallpointHeight: 10,
-          cowsCount: 1,
-        },
-        {
-          posX: 0,
-          posY: 20,
-          wallpointWidth: 10,
-          wallpointHeight: 10,
-          cowsCount: 2,
-        },
-        {
-          posX: 10,
-          posY: 20,
-          wallpointWidth: 10,
-          wallpointHeight: 10,
-          cowsCount: 3,
-        },
-        {
-          posX: 20,
-          posY: 20,
-          wallpointWidth: 10,
-          wallpointHeight: 10,
-          cowsCount: 4,
-        },
-        {
-          posX: 20,
-          posY: 10,
-          wallpointWidth: 10,
-          wallpointHeight: 10,
-          cowsCount: 5,
-        }
-      ];
-
-      chart.data = data;
-
-      const sensors = [
-        {
-          sensorX: 0,
-          sensorY: 10
-        },
-        {
-          sensorX: 0,
-          sensorY: 20
-        },
-        {
-          sensorX: 10,
-          sensorY: 20
-        },
-        {
-          sensorX: 20,
-          sensorY: 20
-        },
-        {
-          sensorX: 30,
-          sensorY: 20
-        }
-      ];
-
-      for (const pos of sensors) {
-        const sensor = new am4core.Image();
-        sensor.href = 'assets/img/sensor_icon.svg';
-        sensor.valign = 'top';
-        sensor.align = 'right';
-
-        const coords = barnCoordToChartCoord(barnWidth, barnHeight, contentWidth, contentHeight, pos.sensorX, pos.sensorY);
-        sensor.marginRight = coords[0] - (sensor.innerWidth / 4);
-        sensor.marginTop = coords[1] - (sensor.innerHeight / 4);
-
-        sensor.zIndex = 100;
-        chart.tooltipContainer.children.push(sensor);
-        sensor.appear();
-      }
-
-      this.chart = chart;
     });
+  }
+
+  private initializeChart() {
+// Create chart instance
+    this.chart = am4core.create('chartdiv', am4charts.XYChart);
+    this.chart.colors.step = 3;
+
+    // Create axes
+    const xAxis = this.chart.xAxes.push(new am4charts.ValueAxis());
+    xAxis.renderer.minGridDistance = 50;
+    xAxis.min = 0;
+    xAxis.max = barnWidth;
+
+    const yAxis = this.chart.yAxes.push(new am4charts.ValueAxis());
+    yAxis.renderer.minGridDistance = 50;
+    yAxis.min = 0;
+    yAxis.max = barnHeight;
+
+    const bgColor = new am4core.InterfaceColorSet().getFor('background');
+
+    // Create series #1
+    const series = this.chart.series.push(new am4charts.LineSeries());
+    series.dataFields.valueY = PositionNames.posY;
+    series.dataFields.valueX = PositionNames.posX;
+    series.dataFields.value = PositionNames.valueInWP;
+    series.strokeOpacity = 0;
+    series.name = 'Series 1';
+
+    const bullet = series.bullets.push(new am4core.Rectangle());
+    bullet.tooltipText = `X{${PositionNames.posY}} Y{${PositionNames.posX}} : {value.workingValue.formatNumber("#.")} cows`;
+
+    bullet.width = 10;
+    bullet.height = 10;
+
+    series.heatRules.push({
+      target: bullet,
+      property: 'fill',
+      minValue: 0,
+      maxValue: 20,
+      min: am4core.color(bgColor),
+      max: this.chart.colors.getIndex(0)
+    });
+
+    bullet.adapter.add('pixelHeight', (pixelHeight, target) => {
+      const dataItem: any = target.dataItem;
+
+      // tslint:disable-next-line:max-line-length
+      const scalers = this.barnToChartScalers(barnWidth, barnHeight, dataItem.dataContext.wallpointWidth, dataItem.dataContext.wallpointHeight);
+      const scaledHeight = scalers[1] * this.contentHeight;
+
+      return scaledHeight;
+    });
+
+    bullet.adapter.add('pixelWidth', (pixelWidth, target) => {
+      const dataItem: any = target.dataItem;
+
+      // tslint:disable-next-line:max-line-length
+      const scalers = this.barnToChartScalers(barnWidth, barnHeight, dataItem.dataContext.wallpointWidth, dataItem.dataContext.wallpointHeight);
+      const scaledWidth = scalers[0] * this.contentWidth;
+
+      return scaledWidth;
+    });
+  }
+
+  private drawSensors() {
+    for (const pos of this.sensors) {
+      const sensor = new am4core.Image();
+      sensor.href = 'assets/img/sensor_icon.svg';
+      sensor.valign = 'top';
+      sensor.align = 'right';
+
+      const coords = this.barnCoordToChartCoord(barnWidth, barnHeight, this.contentWidth, this.contentHeight, pos.sensorX, pos.sensorY);
+      sensor.marginRight = coords[0] - (sensor.innerWidth / 4);
+      sensor.marginTop = coords[1] - (sensor.innerHeight / 4);
+
+      sensor.zIndex = 100;
+      this.chart.tooltipContainer.children.push(sensor);
+      sensor.appear();
+    }
+  }
+
+  private initializeData() {
+
+    this.data = [
+      {
+        // from lower left corner
+        posX: 0,
+        posY: 10,
+        wallpointWidth: 20,
+        wallpointHeight: 10,
+        cowsCount: 1,
+      },
+      {
+        posX: 0,
+        posY: 20,
+        wallpointWidth: 10,
+        wallpointHeight: 10,
+        cowsCount: 2,
+      },
+      {
+        posX: 10,
+        posY: 20,
+        wallpointWidth: 10,
+        wallpointHeight: 10,
+        cowsCount: 3,
+      },
+      {
+        posX: 20,
+        posY: 20,
+        wallpointWidth: 10,
+        wallpointHeight: 10,
+        cowsCount: 4,
+      },
+      {
+        posX: 20,
+        posY: 10,
+        wallpointWidth: 10,
+        wallpointHeight: 10,
+        cowsCount: 5,
+      }
+    ];
+  }
+
+  private initializeSensors() {
+    this.sensors = [
+      {
+        sensorX: 0,
+        sensorY: 10
+      },
+      {
+        sensorX: 0,
+        sensorY: 20
+      },
+      {
+        sensorX: 10,
+        sensorY: 20
+      },
+      {
+        sensorX: 20,
+        sensorY: 20
+      },
+      {
+        sensorX: 30,
+        sensorY: 20
+      }
+    ];
   }
 
   ngOnDestroy() {
@@ -207,6 +203,26 @@ export class WorkPointsChartComponent implements OnInit, OnDestroy {
         this.chart.dispose();
       }
     });
+  }
+
+  private barnToChartScalers(bWidth: number, bHeight: number, bX: number, bY: number) {
+    const sX = bX / bWidth;
+    const sY = bY / bHeight;
+
+    return [sX, sY];
+  }
+
+  private barnCoordToChartCoord(bWidth: number, bHeight: number, cWidth: number, cHeight: number, bX: number, bY: number) {
+    // assumes barn coords go from (0,0) in the bottom left, (maxX, maxY) in the top right
+
+    let sX: number;
+    let sY: number;
+    [sX, sY] = this.barnToChartScalers(bWidth, bHeight, bX, bY);
+
+    const cX = cWidth - cWidth * sX;
+    const cY = cHeight - cHeight * sY;
+
+    return [cX, cY];
   }
 }
 
